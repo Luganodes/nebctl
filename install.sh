de874f4a7754459ccabd40b02f0a784f637c639a
v15hv4 <vishva2912@gmail.com>
2022-08-20 03:29:36 +0530

    Change ownership of install directory to the invoking user

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 INSTALL_DIR=/opt/nebula-control
 REPO_URL=https://gitlab.com/lgns-platform-team/nebula-control.git
 
+# clone the repository
 git clone $REPO_URL $INSTALL_DIR
+
+# create required directories
 mkdir -p $INSTALL_DIR/{ca,hosts,store}
+
+# change ownership of the install dir to current user
+chown -R $USER:$USER $INSTALL_DIR
+
+# create symlink to script
 ln -s $INSTALL_DIR/nebula-control /usr/bin/nebula-control
