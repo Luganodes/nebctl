34c0c7f95065b6cade625f9644894d82c240f77a
v15hv4 <vishva2912@gmail.com>
2022-09-04 20:50:14 +0530

    Reserve an IP range for lighthouses while generating nebula IP

--- utils/ip.py
+++ utils/ip.py
@@ -5,6 +5,8 @@ depends on:
   - ipaddress
 """
 
+import numpy as np
+
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from ipaddress import ip_address, ip_network
@@ -13,9 +15,21 @@ from .db import engine
 from .db.models import Host
 
 # get latest IP matching the given range (if any)
-def generate_nebula_IP(network="0.0.0.0/24"):
+def generate_nebula_IP(net_ip="0.0.0.0", net_mask="255.255.0.0", lighthouse=False):
     with Session(engine) as session:
         used_IPs_query = select(Host.nebula_ip)
         used_IPs = set(map(lambda ip: int(ip_address(ip)), session.scalars(used_IPs_query)))
-        possible_IPs = set(map(int, ip_network(network).hosts()))
-        return ip_address(sorted(possible_IPs - used_IPs)[0])
+        possible_IPs = set(map(int, ip_network(f"{net_ip}/{net_mask}").hosts()))
+        available_IPs = np.array(sorted(possible_IPs - used_IPs), dtype=int)
+
+        # reserve first 255 addresses for lighthouses
+        if lighthouse:
+            target_IPs = available_IPs[available_IPs < (min(possible_IPs) + 255)]
+        else:
+            target_IPs = available_IPs[available_IPs >= (min(possible_IPs) + 255)]
+
+        # throw error if all IPs exhausted
+        if len(target_IPs) == 0:
+            raise Exception(f"Unable to autogenerate nebula IP: range exhausted!")
+
+        return str(ip_address(int(target_IPs[0])))
