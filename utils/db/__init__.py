0e89282b813064f7ee3c01a9f28cbe08fea5de91
v15hv4 <vishva2912@gmail.com>
2022-10-09 16:36:17 +0530

    Fix global database path

--- utils/db/__init__.py
+++ utils/db/__init__.py
@@ -1,10 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from .models import Base
 
+NEBULA_CONTROL_DIR = os.environ.get("NEBULA_CONTROL_DIR")
 DB_PATH = "store/db.sqlite"
 
 # connect to the database
-engine = create_engine(f"sqlite:///{DB_PATH}")
+engine = create_engine(f"sqlite:///{NEBULA_CONTROL_DIR}/{DB_PATH}")
 
 # generate schema
 Base.metadata.create_all(engine)
