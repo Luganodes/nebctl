a2c80c9b218fb335aa2c8caa2739e8bf14e87531
v15hv4 <vishva2912@gmail.com>
2022-10-09 17:45:48 +0530

    Add conditional installation of nebula binaries to install script

--- install.sh
+++ install.sh
@@ -6,7 +6,12 @@ REPO_URL=
 # clone the repository
 git clone $REPO_URL $INSTALL_DIR
 
-# install dependencies
+# install nebula if not present already
+if ! command -v nebula &> /dev/null; then
+    wget -c "https://github.com/slackhq/nebula/releases/download/v1.6.0/nebula-linux-amd64.tar.gz" -O - | sudo tar -xz -C /usr/bin/
+fi
+
+# install python dependencies
 pip install -r $INSTALL_DIR/requirements.txt
 
 # create required directories
